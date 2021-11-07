1086de6c13fcae9b364666aaabc7d3d7a586c2aa
liza <gorbunova.elizaveta@gmail.com>
2021-11-07 16:26:34 +0200

    check routes

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Welcome to news!
+    <p>Welcome to news!</p>
     <v-btn :to="{name: 'news-id', params: {id: 1}}">Go to news with id equal to 1</v-btn>
   </div>
 </template>
