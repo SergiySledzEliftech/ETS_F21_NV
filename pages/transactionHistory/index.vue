647ce64efff3818183435d8dc011bca981083b27
liza <gorbunova.elizaveta@gmail.com>
2021-11-18 17:38:17 +0200

    different requests

--- pages/transactionHistory/index.vue
+++ pages/transactionHistory/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <div>Here will lay chart component </div>
-    <v-btn @click="getTransactionHistory">Click me</v-btn>
-    <v-data-table
-    :headers="headers"
-    :items="transactions"
-    class="elevation-1"
-    ></v-data-table>
+    <v-row align="center">
+      <v-col class="d-flex" cols="6" sm="6">
+        <v-select class="my-6" :items="currencies" v-model="currency" label="Select currency!" dense></v-select>
+      </v-col>
+    </v-row>
+    <v-data-table :headers="headers" :items="transactions" class="elevation-1" hide-default-footer></v-data-table>
+    <v-pagination v-model="page" :length="6" @input="next"></v-pagination>
   </div>
 </template>
 
 <script>
-import { Component, namespace, Vue } from 'nuxt-property-decorator'
+import { Component, namespace, Vue, Watch } from 'nuxt-property-decorator'
 
 const { State, Action } = namespace('transactionHistory');
 
@@ -20,6 +21,10 @@ const { State, Action } = namespace('transactionHistory');
 export default class DashboardPage extends Vue{
   @State transactions
   @Action fetchTransactions
+
+  page = 1
+  currency = 'USD';
+  currencies = ['UAH', 'EUR', 'USD'];
 
   headers = [{ text: 'Currency', value: 'currencyName' },
              { text: 'Amount', value: 'amount' },
@@ -31,7 +36,21 @@ export default class DashboardPage extends Vue{
     }
   }
 
+  next(page) {}
+
+  onSelectCurrency(e) {
+    console.log(e);
+  }
+
+  @Watch('page')
+  @Watch('currency') 
   async getTransactionHistory() {
+    const page = this.page;
+    const currency = this.currency;
+    await this.fetchTransactions({ currency, page, limit: 2 });
+  }
+
+  async mounted() {
     await this.fetchTransactions();
   }
 
--- store/transactionHistory.js
+++ store/transactionHistory.js
@@ -3,12 +3,20 @@ export const state = () => ({
 })
 
 export const actions = {
-  async fetchTransactions (ctx) { // }, { userId }) {
+  async fetchTransactions (ctx, params = {}) {
+    const defaultParams = { currency: 'USD', page: 1, limit: 2 }
+    for (const param in defaultParams) {
+      if (!Object.prototype.hasOwnProperty.call(params, param)) {
+        params[param] = defaultParams[param]
+      }
+    }
     try {
-      const response = await this.$axios.$get('http://localhost:4000/transaction-history')
+      const url = `http://localhost:4000/transaction-history?currency=${params.currency}&page=${params.page}&limit=${params.limit}`
+      console.log(url)
+      const response = await this.$axios.$get(url)
       ctx.commit('updateTransactionHistory', response)
     } catch (error) {
-      // console.log(error)
+      console.error(error)
     }
   }
 }
